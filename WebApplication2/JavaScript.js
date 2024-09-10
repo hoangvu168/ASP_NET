eb0d528aefc16e2cca987dc59a47dcf49428ade7
qv <qv@DESKTOP-J5A321E>
2024-09-10 19:36:05 +0700

    update

---
+++ WebApplication2/JavaScript.js
@@ -0,0 +1,39 @@
+﻿$(document).ready(function () {
+    function sendData() {
+        var soGioChoi = $('#gio').val(); 
+        var giaLoaiGay = $('input[name="LoaiGay"]:checked').val(); 
+        var themDoa = $('#doa').is(':checked') ? $('#doa').val() : 0;
+        if (isNaN(soGioChoi) || soGioChoi <= 0) {
+            alert('Vui lòng nhập số giờ chơi hợp lệ.');
+            return;
+        }
+        if (!giaLoaiGay) {
+            alert('Vui lòng chọn loại gậy.');
+            return;
+        }
+
+        var dataToSend = {
+            gay: giaLoaiGay,
+            gio: soGioChoi, 
+            doa: themDoa,
+        };
+
+        $.ajax({
+            url: "WebForm1.aspx",
+            type: "POST",
+            data: JSON.stringify(dataToSend),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (response) {
+                alert('Tổng thiệt hại là: ' + response);
+            },
+            error: function (xhr, status, error) {
+                alert('Có lỗi xảy ra: ' + xhr.responseText);
+            }
+        });
+    }
+
+    $("#tinhtien").click(function () {
+        sendData();
+    });
+});
---
+++ WebApplication2/WebForm1.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+using ClassLibrary1;
+namespace WebApplication2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.HttpMethod == "POST")
+            {
+                bool themdoa;
+                try
+                {
+                    string jsonString = new System.IO.StreamReader(Request.InputStream).ReadToEnd();
+
+                    var data = JsonConvert.DeserializeObject<Dictionary<string, double>>(jsonString);
+
+                    double gay = data["gay"];
+                    double gio = data["gio"];
+                    double doa = data["doa"];
+                    LoaiCo laoigay = (gay == 1) ? LoaiCo.Vip : LoaiCo.Thuong;
+                    if (doa == 1)
+                    {
+                        themdoa = true;
+                    }
+                    else
+                    {
+                        themdoa= false;
+                    }
+                    double result = Class1.TinhTongTien(laoigay, gio, themdoa);
+                    Response.ContentType = "application/json";
+                    Response.Write(JsonConvert.SerializeObject(result));
+                }
+                catch (Exception ex)
+                {
+                    Response.Write("Lỗi: " + ex.Message);
+                }
+            }
+        }
+    }
+}
